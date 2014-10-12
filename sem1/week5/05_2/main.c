a7907727e08c3d785c485561477885ccfaac710f
Adnan Mahmud <km.adnan@gmail.com>
2014-10-12 16:23:17 +0200

    das Output formattiert

--- sem1/week5/05_2/main.c
+++ sem1/week5/05_2/main.c
@@ -8,22 +8,23 @@ int main(int argc, char **argv)
 	int i=0;
 	int j=0;
 	int k=0;
+	const char asterisk='*';
 	
-	printf ("* ");
+	printf ("%3c ",asterisk);
 	
 	for (i=0;i<LIMIT;i++)
 	{
-	  printf ("%d ",horizontal[i]);
+	  printf ("%3d ",horizontal[i]);
 	 }
 	
 	  printf("\n");
 	
 	for (j=0;j<10;j++)
     {
-      printf ("%d ",vertical[j]);
+      printf ("%3d ",vertical[j]);
           for (k=0;k<10;k++)
           {
-           printf ("%d ",vertical[j]*horizontal[k]);
+           printf ("%3d ",vertical[j]*horizontal[k]);
           }
 	  printf ("\n");
      }
